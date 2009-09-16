9671a461e0b426166cad1f791e3a4c955bfe440d
Vincent Brouillet <vincent.brouillet@gmail.com>
2009-09-16 14:49:55 +0200

    Fix rating ask for login. Closes #8

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,11 @@ class User < ActiveRecord::Base
 
   validates_format_of       :first_name,     :with => Authentication.name_regex,  :message => Authentication.bad_name_message, :allow_nil => true
   validates_length_of       :first_name,     :maximum => 100
+  validates_presence_of     :first_name
 
   validates_format_of       :last_name,     :with => Authentication.name_regex,  :message => Authentication.bad_name_message, :allow_nil => true
   validates_length_of       :last_name,     :maximum => 100
+  validates_presence_of     :last_name
 
   validates_presence_of     :email
   validates_length_of       :email,    :within => 6..100 #r@a.wk
