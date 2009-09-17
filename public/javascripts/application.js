60b7f21099a1aa1f2028a385af1ba09aba4fd323
Vincent Brouillet <vincent.brouillet@gmail.com>
2009-09-17 14:52:31 +0200

    Trying fixing ajax pagination spinner

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,32 +1,30 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-document.observe("dom:loaded", function() {
-  // the element in which we will observe all clicks and capture
-  // ones originating from pagination links
-  var container = $(document.body)
 
-  if (container) {
-//    var img = new Image
-//    img.src = '/images/spinner.gif'
-//
-//    function createSpinner() {
-//      new Element('img', { src: img.src, 'class': 'spinner' })
-//    }
+    document.observe("dom:loaded", function() { 
 
-    container.observe('click', function(e) {
-      var el = e.element()
-      if (el.match('.pagination a')) {
-          
-        //el.up('.pagination').insert(createSpinner())
-        Element.show('spinner')
-        new Ajax.Request(el.href, { method: 'get' })
-        e.stop()
-      }
+        // the element in which we will observe all clicks and capture
+        // ones originating from pagination links
+        var container = $(document.body)
+        if (container) {
+            var spinnerImg = new Element('img', { src: '/images/spinner.gif', 'class': 'spinner' });
+
+            container.observe('click', function(e) {
+                var el = e.element()
+                if (el.match('.pagination a')) {
+                    el.up('.pagination').insert(spinnerImg)
+                    new Ajax.Request(el.href, {
+                        method: 'get'
+                    })
+                    e.stop()
+                }
+           })
+        }
+        
     })
-  }
-})
+
 
 function remove_field(element, item) {
-  element.up(item).remove();
+    element.up(item).remove();
 }
