f09df5f811ecce94fe0226cd900fee3489a76dd7
Vincent Brouillet <vincent.brouillet@gmail.com>
2009-09-24 14:50:39 +0200

    Fix user_observer bug presence email condition. Activation emails are sent again.

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -3,14 +3,14 @@ class UserMailer < ActionMailer::Base
     setup_email(user)
     @subject    += 'Please activate your new account'
   
-    @body[:url]  = "http://YOURSITE/user_activate/#{user.activation_code}"
+    @body[:url]  = "#{ENV['SITE_URL']}/user_activate/#{user.activation_code}"
   
   end
   
   def activation(user)
     setup_email(user)
     @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://YOURSITE/"
+    @body[:url]  = "#{ENV['SITE_URL']}/"
   end
   
   protected
--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,11 +1,11 @@
 class UserObserver < ActiveRecord::Observer
   def after_create(user)
     user.reload
-    UserMailer.deliver_signup_notification(user) if !user.email==""
+    UserMailer.deliver_signup_notification(user) if user.email!=""
   end
 
   def after_save(user)
     user.reload
-    UserMailer.deliver_activation(user) if user.recently_activated? and !user.email==""
+    UserMailer.deliver_activation(user) if user.recently_activated? and user.email!=""
   end
 end
