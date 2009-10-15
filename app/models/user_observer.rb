bf36fc43957d74f4e35ee08b495854a769c01aee
Vincent Brouillet <vincent.brouillet@gmail.com>
2009-10-15 13:40:03 +0200

    Still intends to send emails to users with empty email address

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,11 +1,11 @@
 class UserObserver < ActiveRecord::Observer
   def after_create(user)
     user.reload
-    UserMailer.deliver_signup_notification(user) if user.email!=""  unless user.email.nil?
+    UserMailer.deliver_signup_notification(user) unless user.email==""
   end
 
   def after_save(user)
     user.reload
-    UserMailer.deliver_activation(user) if user.recently_activated? and user.email!=""  unless user.email.nil?
+    UserMailer.deliver_activation(user) if user.recently_activated?  unless user.email==""  unless user.email.nil?
   end
 end
