07296731e44ea5c9a1489aab728155f5c06ee57b
Vincent Brouillet <vincent.brouillet@gmail.com>
2009-10-15 15:41:00 +0200

    Fix sending email to user without email like facebook users

--- app/models/organisms_user_observer.rb
+++ app/models/organisms_user_observer.rb
@@ -5,7 +5,7 @@ class OrganismsUserObserver < ActiveRecord::Observer
       #for each _organism_moderator and organism_admin, send signup notification email
       @organism_admins_or_modo = organisms_user.organism.admins + organisms_user.organism.moderators
       @organism_admins_or_modo.each  do |admin_or_modo|
-        OrganismsUserMailer.deliver_creation_notification(admin_or_modo, organisms_user.user, organisms_user.organism, organisms_user.role) if organisms_user.pending? unless user.email==""
+        OrganismsUserMailer.deliver_creation_notification(admin_or_modo, organisms_user.user, organisms_user.organism, organisms_user.role) if organisms_user.pending? unless admin_or_modo.email==""
       end
   end
 
