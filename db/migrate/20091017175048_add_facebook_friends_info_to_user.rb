25ef3819bd51d1433911157b5fc2ecbb549a7b09
Vincent Brouillet <vincent.brouillet@gmail.com>
2009-10-19 23:12:51 +0200

    Implemented delayed_job. But give up with features around friends who haven\'t accepted SCEM on Facebook. There is no possiblity to interact with them.

--- db/migrate/20091017175048_add_facebook_friends_info_to_user.rb
+++
@@ -1,9 +0,0 @@
-class AddFacebookFriendsInfoToUser < ActiveRecord::Migration
-  def self.up
-    add_column :users, :facebook_friends_info, :text
-  end
-
-  def self.down
-    remove_column :users, :facebook_friends_info
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091017175048) do
+ActiveRecord::Schema.define(:version => 20091017172903) do
 
   create_table "activities", :force => true do |t|
     t.string   "name"
@@ -234,7 +234,6 @@ ActiveRecord::Schema.define(:version => 20091017175048) do
     t.string   "fb_image_small"
     t.boolean  "receive_comment_notification",                :default => true
     t.boolean  "receive_picture_notification",                :default => true
-    t.text     "facebook_friends_info"
   end
 
   add_index "users", ["login"], :name => "index_users_on_login", :unique => true
