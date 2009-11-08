2f4b51d1c250831c1d1c8d77c1c896660556c4e5
Vincent Brouillet <vincent.brouillet@gmail.com>
2009-11-08 20:05:24 +0100

    Side diary call mini_width images to limit the width, not the height

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -4,17 +4,25 @@ module EventsHelper
     "36px"
   end
 
+  def get_mini_width
+    "36px"
+  end
+
   def display_event_cover(event, style)
     if event.picture.nil?
       link_to(get_default_event_picture(style), event)
     else
       if style == "mini"
         link_to(image_tag(event.picture.attached.url(:small), :height => get_mini_height), event)
+      elsif style == "mini_width"
+        link_to(image_tag(event.picture.attached.url(:small), :width => get_mini_height), event)
       else
         link_to(image_tag(event.picture.attached.url(style)), event)
       end
     end
   end
+
+
 
   def display_term_cover(term, style)
     if term.event.picture.nil?
@@ -22,6 +30,8 @@ module EventsHelper
     else
       if style == "mini"
         link_to(image_tag(term.event.picture.attached.url(:small), :height => get_mini_height), url_for_even_polymorphic(term))
+      elsif style == "mini_width"
+        link_to(image_tag(term.event.picture.attached.url(:small), :width => get_mini_height), url_for_even_polymorphic(term))
       else
         link_to(image_tag(term.event.picture.attached.url(style)), url_for_even_polymorphic(term))
       end
@@ -31,6 +41,8 @@ module EventsHelper
   def get_default_event_picture(style)
     if style == "mini"
       image_tag("default/event/small/1.jpg", :height => get_mini_height)
+    elsif style == "mini_width"
+      image_tag("default/event/small/1.jpg", :width => get_mini_width)
     else
       image_tag("default/event/#{style}/1.jpg")
     end
