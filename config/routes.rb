dd1ba5b46a365e1a1818f64497e2ea9dc3143574
Vincent Brouillet <vincent.brouillet@gmail.com>
2009-11-08 17:40:31 +0100

    Add Information page

--- config/routes.rb
+++ config/routes.rb
@@ -112,10 +112,13 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :participations, :collection =>{
     :create_or_update => :get }
 
+  map.information '/information', :controller => 'information', :action => 'index'
+
   #deprecated
   map.resources :terms
 
   map.resources :activities
+
 
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
