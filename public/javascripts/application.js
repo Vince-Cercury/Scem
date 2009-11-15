37c27f7ef20503792159890e3f37ab94a0288efa
Vincent Brouillet <vincent.brouillet@gmail.com>
2009-11-15 22:06:32 +0100

    Deactivate ajax pagination. Reason: back and bookmark button need to be fixed and we need to find a way to redirect to the top of the page. Closes #80

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -22,25 +22,25 @@ function event_copy_start_date_to_end_date(start_date){
         $('event_new_term_attributes__end_at').value = start_date
 }
 
-    document.observe("dom:loaded", function() { 
-        // the element in which we will observe all clicks and capture
-        // ones originating from pagination links
-        var container = $(document.body)
-        if (container) {
-            var spinnerImg = new Element('img', { src: '/images/spinner.gif', 'class': 'spinner' });
-
-            container.observe('click', function(e) {
-                var el = e.element()
-                if (el.match('.pagination a')) {
-                    el.up('.pagination').insert(spinnerImg)
-                    new Ajax.Request(el.href, {
-                        method: 'get'
-                    })
-                    e.stop()
-                }
-           })
-        }
-        
-    })
+//    document.observe("dom:loaded", function() {
+//        // the element in which we will observe all clicks and capture
+//        // ones originating from pagination links
+//        var container = $(document.body)
+//        if (container) {
+//            var spinnerImg = new Element('img', { src: '/images/spinner.gif', 'class': 'spinner' });
+//
+//            container.observe('click', function(e) {
+//                var el = e.element()
+//                if (el.match('.pagination a')) {
+//                    el.up('.pagination').insert(spinnerImg)
+//                    new Ajax.Request(el.href, {
+//                        method: 'get'
+//                    })
+//                    e.stop()
+//                }
+//           })
+//        }
+//
+//    })
 
 
