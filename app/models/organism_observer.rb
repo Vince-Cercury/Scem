dc9d3bcecd243570c2aa4c9630d0d63ff4d11892
Vince <vincent.brouillet@gmail.com>
2009-12-10 12:52:01 +0100

    Trying to fix smtp bad recipient bug

--- app/models/organism_observer.rb
+++ app/models/organism_observer.rb
@@ -9,7 +9,7 @@ class OrganismObserver < ActiveRecord::Observer
 
     #for each system_moderator and system_admin, send signup notification email (contening all organism infos)
     @system_admins_or_modo = User.find(:all, :conditions => ["role = ? or role = ?", "admin", "moderator"] )
-    raise @system_admins_or_modo.inspect
+    #raise @system_admins_or_modo.inspect
     @system_admins_or_modo.each  do |user|
       OrganismMailer.deliver_signup_notification_to_system_admin_or_modo(user, organism) if organism.pending? unless user.email==""
     end
