6b8dff24103f6da9a0d59cd3e7fe7258677d593e
Vince <vincent.brouillet@gmail.com>
2009-12-14 14:58:09 +0100

    Fix wrong path participation email notification. Closes #237

--- app/models/participation_mailer.rb
+++ app/models/participation_mailer.rb
@@ -7,9 +7,9 @@ class ParticipationMailer < ActionMailer::Base
     #organism_admin and organism_modo, send this emails
     setup_email(admin_or_modo,user, event, role)
     @subject    += I18n.t('participation_mailer.subject_participation_notification',:event => event.name)
-    @body[:url_event]  = "#{ENV['SITE_URL']}/events/#{event.id}"
+    @body[:url_event]  = "#{ENV['SITE_URL']}#{url_for_even_polymorphic(term)}"
     @body[:url_user]  = "#{ENV['SITE_URL']}/users/#{user.id}"
-    @body[:url_participants]  = "#{ENV['SITE_URL']}/participations/?term_id=#{term.id}"
+    @body[:url_participants]  = "#{ENV['SITE_URL']}#{event_term_participations_path(:event_id =>event.id, :term_id => term.id, :role =>:sure)}"
   end
 
   def creation_notification_to_sys(admin_or_modo, user, event, term, role)
@@ -17,9 +17,9 @@ class ParticipationMailer < ActionMailer::Base
     #organism_admin and organism_modo, send this emails
     setup_email(admin_or_modo,user, event, role)
     @subject    += I18n.t('participation_mailer.subject_participation_notification',:event => event.name)
-    @body[:url_event]  = "#{ENV['SITE_URL']}/events/#{event.id}"
+    @body[:url_event]  = "#{ENV['SITE_URL']}#{url_for_even_polymorphic(term)}"
     @body[:url_user]  = "#{ENV['SITE_URL']}/users/#{user.id}"
-    @body[:url_participants]  = "#{ENV['SITE_URL']}/participations/?term_id=#{term.id}"
+    @body[:url_participants]  = "#{ENV['SITE_URL']}#{event_term_participations_path(:event_id =>event.id, :term_id => term.id, :role =>:sure)}"
   end
 
 
